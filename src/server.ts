6c9d28ca22c2eb41d5312774c6d0aa2c95095b9a
Jonny <jonik0744clash@email.com>
2026-06-03 14:40:17 +0900

    feat: create routing and member controller

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,8 @@ import app from "./app";
 
 mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3000;
+    const PORT = process.env.PORT ?? 3006;
     app.listen(PORT, function () {
-        console.log(`The server is running successfully on port: ${PORT}`);
+        console.log(`The server is running successfully on port: http://localhost:${PORT}`);
     })
 }).catch( (err) => console.log("ERROR on connection MongoDB", err));
