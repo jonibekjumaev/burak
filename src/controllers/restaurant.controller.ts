67a3e116c30070b258a4f56483c5dfe0f06092aa
Jonny <jonik0744clash@email.com>
2026-06-03 15:27:32 +0900

    feat: member service model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import Memberservice from "../models/Member.service";
 
 const restaurantController: T = {};
 restaurantController.goHome = ((req: Request, res: Response) => {
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from "mongoose";
 import app from "./app";
 
-mongoose.connect(process.env.MONGO_URL as string, {}).then((data) => {
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3006;
     app.listen(PORT, function () {
