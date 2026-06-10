4b13e2477d6ed1eb3beed41e83b179e0567c2aff
Jonny <jonik0744clash@email.com>
2026-06-10 15:00:18 +0900

    feat: develop admin frontend pages and integrate header & footer

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,37 @@
+/* 
+TASK Q:
+Shunday function yozing, u 2 ta parametrga ega bo'lib
+birinchisi object, ikkinchisi string bo'lsin.
+Agar qabul qilinayotgan ikkinchi string, objectning
+biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "model"); return true;
+Ushbu misolda, 'model' string, objectning propertysiga mos kelganligi uchun 'true' natijani qaytarmoqda.
+
+*/
+
+//Yechim:
+
+function hasProperty(obj: object, key: string): boolean {
+    return key in obj;
+}
+
+const result = hasProperty({ name: "BMW", model:"M3" }, "model");
+console.log("natija:", result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*  Project standarts:
     -Logging standarts
     -Naming standarts
