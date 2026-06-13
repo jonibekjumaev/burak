d7f8ff369b0c177b727bd3a4a4fd474c4c59c007
Jonny <jonik0744clash@email.com>
2026-06-13 15:14:08 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Restaurant */
 
@@ -10,12 +11,15 @@ routerAdmin.get("/", restaurantController.goHome);
 
 routerAdmin
     .get("/login", restaurantController.getLogin)
-    .post("/login", restaurantController.processLogin);
+    .post("/login",restaurantController.processLogin);
 
 
 routerAdmin
     .get("/signup", restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+    .post(
+        "/signup",
+        makeUploader("members").single("memberImage"), 
+        restaurantController.processSignup);
 routerAdmin.get("/logout", restaurantController.logout);
 
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
@@ -27,10 +31,13 @@ routerAdmin.get(
     "/product/all",
     restaurantController.verifyRestaurant,
     productController.getAllProducts);
+
 routerAdmin.post(
     "/product/create",
     restaurantController.verifyRestaurant,
+    makeUploader("products").array("productImages", 5),
     productController.createNewProduct);
+
 routerAdmin.post(
     "/product/:id",
     restaurantController.verifyRestaurant,
