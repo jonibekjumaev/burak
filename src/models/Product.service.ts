25200c06fb05fde630b4c554a9a996feb997a36f
Jonny <jonik0744clash@email.com>
2026-06-16 01:49:00 +0900

    fix: getAllProducts api and passing data into ejs

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -17,29 +17,42 @@ class ProductService  {
 
     /** SSR */
 
-public async createNewProduct( input: ProductInput ): Promise<Product> {
-    
-    try {
-        return await this.productModel.create(input);
+    public async getAllProducts(): Promise<Product[]> {
+        const result = await this.productModel.find().exec();
+        if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
-    } catch (err) {
-        console.error("Error, model: createNewProduct:", err);
-        throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        return result;
+
     }
 
-}
-
-public async updateChosenProduct(id: string, input: ProductUpdateInput): Promise<Product> {
-    // string => ObjectId
-    id = shapeIntoMongooseObjectId(id);
-    const result = await this.productModel.findOneAndUpdate({ id: id }, input, { new: true }).exec();
-
-    if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
-
-    return result;
 
 
-} 
+
+
+
+    public async createNewProduct( input: ProductInput ): Promise<Product> {
+    
+        try {
+            return await this.productModel.create(input);
+
+        } catch (err) {
+            console.error("Error, model: createNewProduct:", err);
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
+
+    }
+
+    public async updateChosenProduct(id: string, input: ProductUpdateInput): Promise<Product> {
+        // string => ObjectId
+        id = shapeIntoMongooseObjectId(id);
+        const result = await this.productModel.findOneAndUpdate({ id: id }, input, { new: true }).exec();
+
+        if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+        return result;
+
+
+    } 
 
 }
 
