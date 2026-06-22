b5a26e76fda8148dcc273b7324e8a5378ccb7dae
Jonny <jonik0744clash@email.com>
2026-06-22 13:00:17 +0900

    feat: signup process frontend validation

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -37,21 +37,21 @@ productController.createNewProduct = async (req: AdminRequest, res: Response) =>
         if(!req.files?.length) throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
         const data: ProductInput = req.body;
-        data.productImage = req.files?.map((ele) => {
+        data.productImages = req.files?.map((ele) => {
             return ele.path.replace(/\\/g,"/");
         });
 
         await productService.createNewProduct(data);
 
         res.send(
-            `<script> alert("successful creation"); window.location.replace('admin/product/all') </script>`);
+            `<script> alert("successful creation"); window.location.replace('/admin/product/all') </script>`);
 
     } catch (err) {
         console.log("Error, createNewProduct", err);
         const message = 
             err instanceof Errors ? err.message: Message.SOMETHING_WENT_WRONG;
         res.send(
-            `<script> alert("${message}"); window.location.replace('admin/product/all') </script>`);
+            `<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`);
     }
 };
 
