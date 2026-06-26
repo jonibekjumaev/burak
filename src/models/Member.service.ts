08bf89780756390785ba5b9bf4c1b0207499acd1
Jonny <jonik0744clash@email.com>
2026-06-26 16:30:48 +0900

    fix: modify SPA login API process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,7 @@
 import MemberModel from "../schema/Member.model";
 import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/members";
 import Errors, { HttpCode, Message }  from "../libs/Errors";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberType, MemberStatus } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 
@@ -33,11 +33,16 @@ public async signup(input: MemberInput): Promise<Member> {
         //TODO: Consider member status later
         const member = await this.memberModel
         .findOne( 
-            {memberNick: input.memberNick},  // birinchisi FILTER 
-            { memberNick: 1, memberPassword: 1 }  // 2chisi Projection buladi
+            {  memberNick: input.memberNick,
+               memberStatus: { $ne: MemberStatus.DELETE}, //$ne: degani shundan boshqasini top degani
+            },  // birinchisi FILTER 
+            { memberNick: 1, memberPassword: 1, memberStatus: 1 }  // 2chisi Projection buladi
         )
         .exec();
         if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+        else if (member.memberStatus === MemberStatus.BLOCK) {
+            throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
+        }
 
         const isMatch = await bcrypt.compare(
             input.memberPassword,
