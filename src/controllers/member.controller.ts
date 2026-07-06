934b0948a355ea30f0be9830463efa15bc20eb9c
Jonny <jonik0744clash@email.com>
2026-07-06 15:47:50 +0900

    fix: modify  getMemberDetail business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -58,18 +58,35 @@ memberController.login = async (req: Request, res: Response) => {
 }; 
 
 
-memberController.logout = async (req:Request, res:Response) => {
+memberController.logout = async (req:ExtendedRequest, res:Response) => {
     try {
          console.log("logout");
          res.cookie("accessToken", null, {maxAge: 0, httpOnly: true});
          res.status(HttpCode.OK).json({ logout: true});
 
     } catch (err) {
-        console.log("Error, login:", err);
+        console.log("Error, logout:", err);
        if (err instanceof Errors) res.status(err.code).json(err);
        else res.status(Errors.standart.code).json(Errors.standart);
     }
 }
+
+
+
+memberController.getMemberDetail = async (req:ExtendedRequest, res:Response) => {
+    try {
+         console.log("getMemberDetail");
+        const result = await memberService.getMemberDetail(req.member);
+
+        res.status(HttpCode.OK).json(result);
+
+    } catch (err) {
+        console.log("Error, getMemberDetail:", err);
+       if (err instanceof Errors) res.status(err.code).json(err);
+       else res.status(Errors.standart.code).json(Errors.standart);
+    }
+}
+
 
 
 
