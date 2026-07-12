f9f7b213a2d38cd767cc82d48527f327dfba13de
Jonny <jonik0744clash@email.com>
2026-07-12 15:31:37 +0900

    feat: create view schema and service models

---
+++ src/libs/enums/view.enum.ts
@@ -0,0 +1,4 @@
+
+export enum viewGroup {
+    PRODUCT = "PRODUCT",
+} 
---
+++ src/schema/View.model.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+import { viewGroup } from "../libs/enums/view.enum";
+
+
+
+const viewSchema = new Schema ({ 
+    viewGroup: {
+    type: String,
+    enum: viewGroup,
+    required: true
+    },
+
+    memberid: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "Member"
+    },
+
+    viewRefId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+
+  },
+  { timestamps: true }
+
+);
+
+
+
+export default mongoose.model("view", viewSchema);
